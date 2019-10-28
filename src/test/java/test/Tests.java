3f6d48438b92a770373ce341dec9792504d077e9
Matias <matias.albornoz@tsoftlatam.com>
2019-10-28 09:33:47 -0300

    Primer commit del proyecto

---
+++ src/test/java/test/Tests.java
@@ -0,0 +1,98 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import helpers.Helpers;
+import helpers.Screenshooter;
+import helpers.WebDriverManager;
+import pages.PageLogin;
+import pages.PageLogon;
+import pages.PageReservation;
+
+public class Tests{
+	private WebDriver driver;
+	ArrayList<String> tabs;
+	@BeforeMethod
+	public void setUp() {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		//driver.manage().window().fullscreen();
+		//driver.manage().window().setSize(new Dimension(800,600));
+		//driver.manage().window().setPosition(new Point(400,300));
+		driver.navigate().to("http://newtours.demoaut.com/");
+		JavascriptExecutor javaScriptExecutor = (JavascriptExecutor)driver;
+		String googleWindow = "window.open('http://www.google.com')";
+		javaScriptExecutor.executeScript(googleWindow);
+		tabs = new ArrayList<String>(driver.getWindowHandles());
+	}
+	
+	
+	@Test
+	public void pruebaUno() {
+		WebDriverManager.setWindowSize(driver, "maximized");
+		driver.switchTo().window(tabs.get(1)).navigate().to("http://www.youtube.com/user/Draculinio");
+		driver.switchTo().window(tabs.get(0));
+		PageLogin pageLogin = new PageLogin(driver);
+		PageLogon pageLogon = new PageLogon(driver);
+		pageLogin.login("user", "user");
+		pageLogon.assertLogonPage();
+		
+	}
+	
+	@Test
+	public void pruebaDos() {
+		WebDriverManager.setWindowSize(driver, "fullscreen");
+		PageLogin pageLogin = new PageLogin(driver);
+		PageReservation pageReservation = new PageReservation(driver);
+		pageLogin.login("mercury", "mercury");
+		pageReservation.assertPage();
+	}
+	
+	@Test
+	public void pruebaTres() {
+		WebDriverManager.setWindowSize(driver, 400, 400);
+		PageLogin pageLogin = new PageLogin(driver);
+		PageReservation pageReservation = new PageReservation(driver);
+		pageLogin.login("mercury", "mercury");
+		pageReservation.selectPassengers(2);
+		pageReservation.selectFromPort(3);
+		pageReservation.selectToPort("London");
+	}
+	
+	@Test
+	public void pruebaCantidadDeCampos() {
+		PageLogin pageLogin = new PageLogin(driver);
+		pageLogin.verifyFields();
+	}
+	
+	@AfterMethod
+	public void tearDown(ITestResult result) {
+		if (!result.isSuccess()) {
+			
+			Screenshooter.takeScreenshot("Error", driver);
+			
+		}
+		driver.switchTo().window(tabs.get(1)).close();
+		driver.switchTo().window(tabs.get(0)).close();
+	}
+}
